1437de9d27adf0fe39b80af32753e66ab1de18f1
user <hgs.use1822r@gmail.com>
2026-08-16 17:44:05 +0900

    Initial commit

---
+++ src/test/java/com/example/eitango/EitangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eitango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EitangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
